8c5dde3e65ec3446b20f4c7fbf39358f6f12a35e
DamianRondinon <95187328+DamianRondinon@users.noreply.github.com>
2024-03-09 23:07:18 -0300

    Task creation, error handling.

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -4,19 +4,26 @@ export const getAllTasks = (req, res) => res.send("Getting tasks");
 
 export const getTask = (req, res) => res.send("Getting single task");
 
-export const createTask = async (req, res) => {
+export const createTask = async (req, res, next) => {
   // I take the user's data.
   const { title, description } = req.body;
 
   // db insert
-  const { rows } = await pool.query(
-    "INSERT INTO task (title, description) VALUES ($1, $2)",
-    [title, description]
-  );
 
-  console.log(rows);
-
-  res.send("creando tarea");
+  try {
+    const result = await pool.query(
+      "INSERT INTO task (title, description) VALUES ($1, $2) RETURNING *",
+      [title, description]
+    );
+    // We handle errors so that the server continues to function and
+    // doesn't crash, allowing us to continue making requests.
+    res.json(result.rows[0]);
+  } catch (error) {
+    if (error.code == "23505") {
+      return res.send("taks already exists");
+    }
+    next(error);
+  }
 };
 
 export const updateTask = (req, res) => res.send("Updating single task");
