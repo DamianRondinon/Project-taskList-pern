8bdd0d7efaf68da722cc303530b113d0c81079cc
DamianRondinon <95187328+DamianRondinon@users.noreply.github.com>
2024-03-16 20:39:36 -0300

    Starting user registration

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,10 @@
-
 export const signin = (req, res) => res.send("Logging in");
 
-export const signup = (req, res) => res.send("Registering");
+export const signup = (req, res) => {
+  const { name, email, password } = req.body;
+  console.log(name, email, password);
+  return res.send("registrando");
+};
 
 export const signout = (req, res) => res.send("Logging out");
 
